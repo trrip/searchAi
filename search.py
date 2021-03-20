bf4edb80d85ca6d6b9fd3eecc6eb3598888ed43f
Nilesh Choudhary <choudhary.nilesh@media4u.com>
2021-03-20 22:41:59 +0000

    Initial commit

--- search.py
+++ search.py
@@ -208,28 +208,6 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
     return mySolverAstar(problem, [], heuristic)
-    # start = problem.getStartState()
-    # exploredState = []
-    # states = util.PriorityQueue()
-    # states.push((start, []), nullHeuristic(start, problem))
-    # nCost = 0
-    # while not states.isEmpty():
-    #     state, actions = states.pop()
-    #     if problem.isGoalState(state):
-    #         return actions
-    #     if state not in exploredState:
-    #         successors = problem.getSuccessors(state)
-    #         for succ in successors:
-    #             coordinates = succ[0]
-    #             if coordinates not in exploredState:
-    #                 directions = succ[1]
-    #                 nActions = actions + [directions]
-    #                 nCost = problem.getCostOfActions(nActions) + heuristic(
-    #                     coordinates, problem
-    #                 )
-    #                 states.push((coordinates, actions + [directions]), nCost)
-    #     exploredState.append(state)
-    # return actions
 
 
 # Abbreviations
